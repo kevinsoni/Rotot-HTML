e8c10989e8bcade666a1b38090ca34a4af7e6f3e
kevinsoni <kevinsoni22593@gmail.com>
2022-07-13 01:56:25 +0530

    Section 1 and Section 2 complete

---
+++ script.js
@@ -0,0 +1,9 @@
+$().ready(function () {
+    $('.slick-carousel').slick({
+        arrows: true,
+        centerPadding: "0",
+        dots: false,
+        slidesToShow: 1,
+        infinite: true
+    });
+});
